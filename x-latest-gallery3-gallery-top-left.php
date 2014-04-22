381622a84077adcefd1e36bb7378cf1f10e44ed0
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-04-22 16:21:18 +0800

    Widgetizing

---
+++ x-latest-gallery3-gallery-top-left.php
@@ -0,0 +1,51 @@
+<?
+	widget_css();
+	
+$_bo_table = $widget_config['forum1'];
+$_no_of_posts = $widget_config['default_no_of_posts'];
+if ( empty($_bo_table) ) $_bo_table = $widget_config['default_forum_id'];
+
+$list = g::posts( array(
+			"bo_table" 	=>	$_bo_table,
+			"select"	=>	"idx,domain,bo_table,wr_id,wr_parent,wr_is_comment,wr_comment,ca_name,wr_datetime,wr_hit,wr_good,wr_nogood,wr_name,mb_id,wr_subject,wr_content"
+				)
+		);	
+?>
+
+<div class='latest-top-left'>
+	
+	<?
+	if ( $list ) {
+		$_wr_id = $list[0]['wr_id'];
+		$imgsrc = x::post_thumbnail($_bo_table, $_wr_id, 538, 213);
+		$img = $imgsrc['src'];
+		if ( empty($img) ) {
+			$_wr_content = db::result("SELECT wr_content FROM $g5[write_prefix]$_bo_table WHERE wr_id='$_wr_id'");
+			$image_from_tag = g::thumbnail_from_image_tag( $_wr_content, $_bo_table, 538, 213 );
+			if ( empty($image_from_tag) ) $image_from_tag = g::thumbnail_from_image_tag( "<img src='$latest_skin_url/img/no_image.png'/>", $_bo_table, 538, 213 );
+			$img = $image_from_tag;
+		}
+	}
+	else $img = $latest_skin_url."/img/default_banner.png";
+	?>
+	
+	<div class='top-left'>
+			<? if ( $list ) {
+					$url = $list[0]['url'];
+					$subject = cut_str($list[0]['wr_subject'],10,'...');
+					$content = cut_str($list[0]['wr_content'], 80, '...');
+			}
+			else {
+				$url = "javascript:void(0);";
+				$subject = "회원님께서는 현재";
+				$content = "필고 갤러리 테마 No.2를 선택 하셨습니다.";
+			}
+			?>
+		<img src="<?=$img?>"/>
+		<div class='top-left-container'>
+			<div class='top-left-subject'><?=$subject?></div>
+			<div class='top-left-content'><?=$content?></div>
+		</div>
+		<a href="<?=$url?>" class='read_more'></a>
+	</div>
+</div>
