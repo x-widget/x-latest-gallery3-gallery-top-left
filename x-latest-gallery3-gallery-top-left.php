a5c67d6fa7efc5b5745372b85cabe24d98af238c
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-04-23 19:01:15 +0800

    Fix for No_image

--- x-latest-gallery3-gallery-top-left.php
+++ x-latest-gallery3-gallery-top-left.php
@@ -22,7 +22,7 @@ $list = g::posts( array(
 		if ( empty($img) ) {
 			$_wr_content = db::result("SELECT wr_content FROM $g5[write_prefix]$_bo_table WHERE wr_id='$_wr_id'");
 			$image_from_tag = g::thumbnail_from_image_tag( $_wr_content, $_bo_table, 538, 213 );
-			if ( empty($image_from_tag) ) $image_from_tag = g::thumbnail_from_image_tag( "<img src='".x::url()."widget/$widget_config[name]/img/no_image.png'/>", $_bo_table, 538, 213 );
+			if ( empty($image_from_tag) ) $image_from_tag = g::thumbnail_from_image_tag( "<img src='".x::url()."/widget/$widget_config[name]/img/no_image.png'/>", $_bo_table, 538, 213 );
 			$img = $image_from_tag;
 		}
 	}
